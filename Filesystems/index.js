880ae4a50cab78a5e44819786c8cf78975e7a80f
merajstack <meraj.md5862@gmail.com>
2026-07-27 11:49:01 +0530

    fs writeFile

--- Filesystems/index.js
+++ Filesystems/index.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const fs = require('fs');
-fs.readFile("./novel.txt",(err,data)=>{
-    if (err == null || err == "undefined"){
-        console.log("File Data :",data.toString());
+// fs.readFile("./novel.txt",(err,data)=>{
+//     if (err == null || err == "undefined"){
+//         console.log("File Data :",data.toString());
+//     }
+//     else{
+//         console.log("Error :",err);
+//     }
+// })
+data = "Hello iam Meraj md  , studying in niat"
+data = "Hello iam Meraj   , studying in niat"
+fs.writeFile("./writefiletest.txt",data,(err)=>{
+    if (err == null || err == undefined){
+        console.log("Data added successfully!")
     }
     else{
-        console.log("Error :",err);
+        console.log("error :",err)
     }
-
 })
