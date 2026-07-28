a6dc2f507567260371f170dab0cabbd1e2628211
merajstack <meraj.md5862@gmail.com>
2026-07-28 12:30:58 +0530

    fs 2 stats/stat methods

---
+++ Filesystems-2/index.js
@@ -0,0 +1,19 @@
+// const fs = require("fs");
+
+// fs.stat("./students/student1.txt",(err,stat)=>{
+//     if (err === null|| err === undefined){
+//         console.log(stat)
+//     }
+//     else{
+//         console.log(err)
+//     }
+// })
+
+// fs.stat("./students/student1.txt",(err,stat)=>{
+//     if (err === null|| err === undefined){
+//         console.log(stat.isFile()) // Return Boolean 
+//     }
+//     else{
+//         console.log(err)
+//     }
+// })
